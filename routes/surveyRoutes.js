cd70fbf252442fa84ff78de2c2b0a6b8f4b8578a
Ritwik <ritwik5402@gmail.com>
2025-12-20 19:24:34 +0530

    Users can see survey results

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -4,7 +4,7 @@ const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middllewares/requireLogin');
 const requireCredits = require('../middllewares/requireCredits');
-const Mailer = require('../services/Mailer');
+const Mailer = require('../services/mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
